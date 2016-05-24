917096d8e8b2cdfcc7355019bae765ce5bf5f82c
Julien Bourdeau <julien@sigerr.org>
2016-05-24 15:56:24 +0200

    Git: introduce `git filelog` alias
    (cherry picked from commit 339cbed1094aca82a6dc118e57d0a82613673045)

--- home/gitconfig.sh
+++ home/gitconfig.sh
@@ -38,6 +38,7 @@
   br = branch -avv
   lg = log --graph --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ar)%Creset'
   ll = log --pretty=format:"%C(red)%h%C(auto)%d\\ %C(reset)%s%C(green)\\ [%cn]%C(reset)" --decorate --numstat
+  filelog = log -u -3
 
   # git ignore laravel,osx,phpstorm
   ignore = "!gi() { curl -L -s https://www.gitignore.io/api/$@ >> .gitignore ;}; gi"
