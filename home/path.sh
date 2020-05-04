363bdc7cafa65d4ef65cc21f7a0806726650f4be
Maxime Biloé <maxime.biloe@gmail.com>
2020-05-04 10:21:07 +0200

    Add zsh

--- home/path.sh
+++ home/path.sh
@@ -16,3 +16,10 @@ PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 # Manpages for GNU utils
 MANPATH="/usr/local/opt/findutils/libexec/gnuman:$MANPATH"
 MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=~/Library/Python/3.7/bin:$PATH
